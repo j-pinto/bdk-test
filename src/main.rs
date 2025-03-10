41b0e3f6977f98aae62a1f46168775cb1833ab63
j-pinto <jfpinto1989@gmail.com>
2025-03-09 23:13:26 -0400

    node connect given its own function

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use bdk_bitcoind_rpc::bitcoincore_rpc::{Auth, Client, RpcApi};
 
-fn main() {
+fn connect_to_node() -> Client {
     let rpc_auth = Auth::UserPass(String::from("user"), String::from("password"));
     let rpc_url = String::from("http://192.168.1.2:48332");
     let rpc_client = Client::new(&rpc_url, rpc_auth).unwrap();
+    rpc_client
+}
 
-    println!("{}", rpc_client.get_block_count().unwrap());
+fn main() {
+    let client = connect_to_node();
+    println!("{:?}", client.get_block_count());
 }
 
