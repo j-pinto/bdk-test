15355b8198c9e7d36b69069358b3e20b3f6bb63e
j-pinto <jfpinto1989@gmail.com>
2025-03-09 23:06:40 -0400

    test connection to node via rpc

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use bdk_bitcoind_rpc::bitcoincore_rpc::{Auth, Client, RpcApi};
+
 fn main() {
-    println!("Hello, world!");
+    let rpc_auth = Auth::UserPass(String::from("user"), String::from("password"));
+    let rpc_url = String::from("http://192.168.1.2:48332");
+    let rpc_client = Client::new(&rpc_url, rpc_auth).unwrap();
+
+    println!("{}", rpc_client.get_block_count().unwrap());
 }
+
