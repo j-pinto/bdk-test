bbf1ccc6d8dcbee7ac84b54a3f5389ee93a000e4
j-pinto <jfpinto1989@gmail.com>
2025-03-13 22:51:07 -0400

    generate mnemonic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use bdk_bitcoind_rpc::bitcoincore_rpc::{Auth, Client, RpcApi};
 use bdk_wallet::bitcoin::{key::{self, Secp256k1}, NetworkKind};
+use bdk_wallet::keys::bip39::Mnemonic;
 
 fn connect_to_node() -> Client {
     let rpc_auth = Auth::UserPass(String::from("user"), String::from("password"));
@@ -19,5 +20,13 @@ fn main() {
     let pub_key = priv_key.public_key(&secp);
     println!("public key: {}", pub_key);
 
+    let entropy = priv_key.to_bytes();
+
+    let mnemonic = Mnemonic::from_entropy(&entropy).unwrap();
+    println!("mnemonic: {:?}", mnemonic);
+
+	for (i, word) in mnemonic.words().enumerate() {
+	    println!("{}. {}", i, word);
+	}
 }
 
